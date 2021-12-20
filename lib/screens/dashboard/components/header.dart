23eff0f4187feac10797c3ee15900cf88e4db9e2
VOLKAN AĞAOĞLU <agaoglu.volkan@gmail.com>
2021-12-21 01:56:58 +0300

    Update header.dart

--- lib/screens/dashboard/components/header.dart
+++ lib/screens/dashboard/components/header.dart
@@ -18,7 +18,9 @@ class Header extends StatelessWidget {
         if (!Responsive.isDesktop(context))
           IconButton(
             icon: Icon(Icons.menu),
-            onPressed: context.read<MenuController>().controlMenu,
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
           ),
         if (!Responsive.isMobile(context))
           Text(
